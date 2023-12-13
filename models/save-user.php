9644766d8c2bd3458a0953267a00377ea966b7ba
thiago <thiagosilva327@live.com>
2023-12-13 18:00:41 -0300

    feat: validations and responses for the user [save-user].

--- models/save-user.php
+++ models/save-user.php
@@ -1,17 +1,25 @@
 <?php
-    switch ($_REQUEST["action"]) {
-        case 'create':
-            $name = $_POST["name"];
-            $email = $_POST["email"];
+switch ($_REQUEST["action"]) {
+    case 'create':
+        $name = $_POST["name"];
+        $email = $_POST["email"];
 
-            $sql = "INSERT INTO users (name, email) VALUES ('{$name}', '{$email}')";
+        $sql = "INSERT INTO users (name, email) VALUES ('{$name}', '{$email}')";
 
-            $res = $conn->query($sql);
-            break;
-        case 'edit':
-            # code...
-            break;
-        case 'delete':
-            # code...
-            break;
-    }
+        $res = $conn->query($sql);
+
+        if ($res == true) {
+            print "<script>alert('User successfully registered');</script>";
+            print "<script>location.href='?page=view-users';</script>";
+        }else{
+            print "<script>alert('Unable to register this user');</script>";
+            print "<script>location.href='?page=new-user';</script>";
+        }
+        break;
+    case 'edit':
+        # code...
+        break;
+    case 'delete':
+        # code...
+        break;
+}
