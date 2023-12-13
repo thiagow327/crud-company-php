80737ac0fd29669c13b3f3aa940ac903c3bbecd2
thiago <thiagosilva327@live.com>
2023-12-13 16:00:36 -0300

    feat: navbar with 'Home', 'Users' and 'Sectors' options [Header]

--- index.php
+++ index.php
@@ -12,6 +12,42 @@
 </head>
 
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/images/logo.png" alt="PHP" width="30" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Users
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="new-user.php">New User</a></li>
+                            <li><a class="dropdown-item" href="view-users.php">View Users</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Sectors
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="new-sector.php">New Sector</a></li>
+                            <li><a class="dropdown-item" href="view-sectors.php">View Sectors</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-pzjw8t+4zLX7L3np8F9ltI5lb9sL06aOK6E7Ll5f7Iao00jMn5endoGZ5biSQtQ6" crossorigin="anonymous"></script>
 </body>
 
