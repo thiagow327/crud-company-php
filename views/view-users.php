c2842ad73eea90050993cc7333d0cc46adb318cc
thiago <thiagosilva327@live.com>
2023-12-14 00:27:53 -0300

    feat: table data display [view-users].

--- views/view-users.php
+++ views/view-users.php
@@ -1,1 +1,33 @@
 <h1>View Users</h1>
+<?php
+$sql = "SELECT * FROM users";
+
+$res = $conn->query($sql);
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+    print "<table class='table table-hover table-bordered'>";
+    print "<thead class='table-dark'>";
+    print "<tr>";
+    print "<th>#</th>";
+    print "<th>Name</th>";
+    print "<th>E-mail</th>";
+    print "<th>Actions</th>";
+    print "</tr>";
+    print "</thead>";
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+        print "<td>" . $row->id . "</td>";
+        print "<td>" . $row->name . "</td>";
+        print "<td>" . $row->email . "</td>";
+        print "<td>
+                    <button class='btn btn-secondary'>Edit</button>
+                    <button class='btn btn-danger'>Delete</button>
+                </td>";
+        print "</tr>";
+    }
+    print "</table>";
+} else {
+    print "<p class='No results found</p>";
+}
