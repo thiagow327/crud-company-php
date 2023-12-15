9c01b68bb5f677cdd32fc9a0c26ebacc940c2c92
thiago <thiagosilva327@live.com>
2023-12-14 21:17:11 -0300

    feat: displays the sectors in the sector field [new-user].

--- views/new-user.php
+++ views/new-user.php
@@ -10,6 +10,22 @@
         <input type="email" name="email" class="form-control">
     </div>
     <div class="mb-3">
+        <label>Sector</label>
+        <select class="form-select" aria-label="Default select example">
+            <?php
+            $sql = "SELECT * FROM sectors";
+
+            $res = $conn->query($sql);
+
+            $qtd = $res->num_rows;
+
+            while ($row = $res->fetch_object()) {
+                print "<option>" . $row->name . "</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <div class="mb-3">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 
